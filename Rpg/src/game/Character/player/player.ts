cc337298ec73f2b55652a4bd3ca73db6afae94a5
CodigoDeSenior <migueljnm840@gmail.com>
2021-04-10 15:59:32 -0300

    Remove player directory

--- Rpg/src/game/Character/player/player.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import { Character } from '../character.ts'
-// @ts-ignore
-import { Classes } from '../interfaces/classes.ts'
-
-class Player extends Character{
-    class: Classes;
-}
