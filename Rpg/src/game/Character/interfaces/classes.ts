9d31e811701a5ba4f6018c442422e8988f79bf84
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2021-04-10 15:16:50 -0300

    Remove useless class and add new simple classes

--- Rpg/src/game/Character/interfaces/classes.ts
+++ Rpg/src/game/Character/interfaces/classes.ts
@@ -1,62 +1,29 @@
-// @ts-ignore
-import { d4, d6, d8, d10, d12, d20} from '../../Dice/dice.ts';
-
 interface Classes{
-    hpDice: any,
+    hp: number,
     armor: any,
     weapons: any,
     tools: any,
-    throws: any,
     skills: any
 }
 
-export const Barbarian: Classes = {
-    hpDice: d12(),
-    armor: ["Light Armor", "Medium Armor", "shields" ],
-    weapons: ["Simple Weapons", "Martial Weapons"],
+export const Warrior: Classes = {
+    hp: 25,
+    armor: ['Warrior Armor'],
+    weapons: ['Swords'],
     tools: [],
-    throws: ["Strength", "Constitution"],
-    skills: [ "Animal Handling", "Athletics", "Intimidation", "Nature", "Perception", "Survival"]
+    skills: ['Brutal Cut']
 }
-export const Bard: Classes = {
-    hpDice: d8(),
-    armor: ["Light Armor"],
-    weapons: ["Simple Weapons", "hand crossbows", "longswords", "rapiers", "shortswords"],
-    tools: ["Musical Instruments"],
-    throws: ["Dexterity", "Charisma"],
-    skills: [ "All"]
+export const Archer: Classes = {
+    hp: 25,
+    armor: ['Light Armor'],
+    weapons: ['Bow'],
+    tools: ['Arrows'],
+    skills: ['Quick Arrows']
 }
-export const Cleric: Classes = {
-    hpDice: d8(),
-    armor: ["Light Armor", "Medium Armor", "Shields"],
-    weapons: ["Simple Weapons"],
-    tools: [],
-    throws: ["Wisdom", "Charisma"],
-    skills: ["History", "Insight", "Medicine", "Persuasion", "Religion"]
+export const Mage: Classes = {
+    hp: 25,
+    armor: [],
+    weapons: ['Staff'],
+    tools: ['Magic Stones'],
+    skills: ['Elemental Ball']
 }
-export const Druid: Classes = {
-    hpDice: d8(),
-    armor: ["Light Armor", "Medium Armor", "Shields"],
-    weapons: ["clubs", "daggers", "darts", "javelins", "maces", "quarterstaffs", "scimitars", "sickles", "slings", "spears"],
-    tools: ["Herbalism Kit"],
-    throws: ["Wisdom", "Intelligence"],
-    skills: ["Arcana", "Animal Handling", "Insight", "Medicine", "Nature", "Perception", "Religion", "Survival"]
-}
-
-export const Fighter: Classes = {
-    hpDice: d10(),
-    armor: ["Light Armor", "Medium Armor", "Shields", "Heavy Armor"],
-    weapons: ["Simple Weapons", "Martial Weapons"],
-    tools: [],
-    throws: ["Strength", "Constitution"],
-    skills: ["Acrobatics", "Animal Handling", "Athletics", "History", "Insight", "Intimidation", "Perception", "Survival"]
-}
-export const Monk: Classes = {
-    hpDice: d8(),
-    armor: ["Light Armor", "Medium Armor", "Shields", "Heavy Armor"],
-    weapons: ["Simple Weapons", "Martial Weapons"],
-    tools: [],
-    throws: ["Strength", "Constitution"],
-    skills: ["Acrobatics", "Animal Handling", "Athletics", "History", "Insight", "Intimidation", "Perception", "Survival"]
-}
-
