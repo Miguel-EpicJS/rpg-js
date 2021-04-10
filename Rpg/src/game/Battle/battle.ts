6d08442733b4988269df1532952ce7b874bad54c
CodigoDeSenior <migueljnm840@gmail.com>
2021-04-10 16:46:35 -0300

    Simple Battle class and battle function

---
+++ Rpg/src/game/Battle/battle.ts
@@ -0,0 +1,18 @@
+import { Character } from '../Character/character'
+// @ts-ignore
+import {Character} from '../Character/character.ts'
+
+export class Battle{
+    player: Character;
+    enemy: Character;
+
+    constructor(player: Character, enemy: Character){
+        this.player = player
+        this.enemy = enemy
+    }
+
+    battle(){
+        console.log("Iniciando batalha, você encontrou um ", this.enemy.name);
+        
+    }
+}
