3ce70cb94c1a4ccf7dc0e5be19d39147fbe2ab62
CodigoDeSenior <migueljnm840@gmail.com>
2021-04-17 07:51:03 -0300

    Base server

---
+++ Rpg/src/server/router.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import { Router } from "https://deno.land/x/oak/mod.ts";
+const router = new Router();
+router.get("/", (context) => {
+    context.response.body = "Hello world!";
+});
